22012dbd3cfe37df74244e26a71f6183ac67b4e2
edwardcardy <edward.cardy@mollie.com>
2022-03-21 09:56:52 +0000

    created dim_products stg model

--- greenery/models/example/staging/stg_orders.sql
+++ greenery/models/example/staging/stg_orders.sql
@@ -5,8 +5,17 @@
 }}
 
 SELECT 
-product_id,
-name,
-price,
-inventory
-FROM {{ source('tutorial', 'products') }}
+order_id,
+user_id,
+promo_id,
+address_id,
+created_at,
+order_cost,
+shipping_cost,
+order_total,
+tracking_id,
+shipping_service,
+estimated_delivery_at,
+delivered_at,
+status
+FROM {{ source('tutorial', 'orders') }}
