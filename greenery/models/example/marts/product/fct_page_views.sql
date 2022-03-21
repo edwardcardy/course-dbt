a1ad37b693a3d87f7ad6500a961cd46ea5dd9fe8
edwardcardy <edward.cardy@mollie.com>
2022-03-21 13:14:43 +0000

    created fct_page_views

---
+++ greenery/models/example/marts/product/fct_page_views.sql
@@ -0,0 +1,11 @@
+
+with page_views as (
+    select *
+    from {{ ref('int_page_views') }}
+),
+
+
+    select
+        *,
+        num_events*1.0/num_sessions as avg_views_per_session,
+    from page_views
---
+++ greenery/models/example/marts/product/intermediate/int_page_views.sql
@@ -0,0 +1,19 @@
+
+
+with events as (
+    select *
+    from {{ ref('stg_events') }}
+),
+
+
+
+    select 
+        page_url,
+        count(event_id) as num_events,
+        count(distinct session_id) as num_sessions,
+        min(created_at) as first_viewed_at,
+        max(created_at) as last_viewed_at
+        from events
+    group by 1
+
+
