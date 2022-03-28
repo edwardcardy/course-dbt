027cf96accb4fce61d443e7500d426166db5240a
edwardcardy <edward.cardy@mollie.com>
2022-03-28 17:00:13 +0000

    fixed error

--- greenery/models/example/staging/stg_products.sql
+++ greenery/models/example/staging/stg_products.sql
@@ -7,7 +7,7 @@
 SELECT 
 product_id,
 name AS product_name,
-REPLACE(name,' ','') AS product_name_nospaces,
+REPLACE(REPLACE(name,' ',''), '''', '') AS product_name_nospaces,
 price,
 inventory
 FROM {{ source('tutorial', 'products') }}
