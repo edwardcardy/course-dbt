d4bef629666a4440b006e300d879a216cd70310f
edwardcardy <edward.cardy@mollie.com>
2022-03-28 15:12:24 +0000

    created macro to automate event type aggregation

--- greenery/models/example/marts/product/intermediate/int_session_facts.sql
+++ greenery/models/example/marts/product/intermediate/int_session_facts.sql
@@ -5,18 +5,23 @@
     ) 
 }}
 
+{% set event_types = dbt_utils.get_column_values(table=ref('stg_events'), column='event_type') %}
+
 with events as (
     select * from {{ ref('stg_events') }}
 )
+
+
+
 
 select
     session_id,
     event_created_date,
     user_id,
-    sum(case when event_type = 'page_view' then 1 end) as count_page_view,
-    sum(case when event_type = 'add_to_cart' then 1 end) as count_add_to_cart,
-    sum(case when event_type = 'checkout' then 1 end) as count_checkout,
-    sum(case when event_type = 'package_shipped' then 1 end) as count_package_shipped
+    {% for event_type in event_types %}
+    SUM(CASE WHEN event_type = '{{event_type}}' THEN 1 ELSE 0 END) AS count_{{event_type}}_events
+    {% if not loop.last %},{% endif %}  
+    {% endfor %}
 from events
 
 {{ dbt_utils.group_by(3) }}
